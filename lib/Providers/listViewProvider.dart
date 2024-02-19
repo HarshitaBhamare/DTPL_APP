e1698163deb6c0a3190a165de8b8d103c5486950
RohanGajjar23 <95432467+RohanGajjar23@users.noreply.github.com>
2024-02-19 18:32:06 +0530

    fixed

--- lib/Providers/listViewProvider.dart
+++ lib/Providers/listViewProvider.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class ListViewProvider with ChangeNotifier {
   int currentIndex = 0;
   void SetIndex(int index) {
+    int bkp = currentIndex;
     currentIndex = index;
-    notifyListeners();
+    if (bkp != index) {
+      notifyListeners();
+    }
     // print("Setting Index $index");
   }
 }
